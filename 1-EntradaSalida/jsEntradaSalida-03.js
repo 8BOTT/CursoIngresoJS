c06e6ed65543732f51e4e320a3e85a889e4ad657
8BOTT <aguscastillo1235@gmail.com>
2023-01-31 16:40:08 -0300

    Entrada y salida 1,2,3,4 ok

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,11 +1,16 @@
 /*
 Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'
+Castillo Agustín Div X*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	let nombreIngresado; //variable local
+
+
+nombreIngresado = document.getElementById("txtIdNombre").value;
+
+
+	alert(nombreIngresado);
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,15 @@
 /* 
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
-*/
+Castillo Agustín*/
 function mostrar()
 {
-	alert("ok");
+let nombre;
+
+nombre=prompt ("Ingrese su nombre");
+
+document.getElementById ("txtIdNombre").value = nombre;
+
+	
 }
 
