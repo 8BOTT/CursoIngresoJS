5d4ce2fe87bf28a237c66c43303bb5d0f85f5dac
8BOTT <aguscastillo1235@gmail.com>
2023-01-31 14:18:12 -0300

    Ejercicios 1 y 2 ok

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,9 @@
 /*
 Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"
+Castillo Agustín Div X*/
 function mostrar()
 {
-	alert("Hola");
+	alert("Esto funciona de maravilla");
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,18 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'
+Castillo Agustín Div X*/
 function mostrar()
 {
-	alert("ok");
+let nombre; //declaración de variable
+//asignación
+//nombre = "Agustín"
+
+nombre=prompt("Ingrese su nombre");
+
+	alert(nombre); //"literal"
+
+	
+
 }
 
