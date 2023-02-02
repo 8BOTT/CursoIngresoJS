d338b6ea92f4afcbbe4d639d1aeb4b1b3c2b050e
8BOTT <aguscastillo1235@gmail.com>
2023-02-02 14:28:31 -0300

    Entrada y Salida Completos

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,23 +1,23 @@
-/*
-Debemos lograr tomar el importe por ID ,
-transformarlo a entero (parseInt), luego
-mostrar el importe con un aumento del 10 %
-en el cuadro de texto "RESULTADO".
-Castillo Agustín Div X*/
-function mostrarAumento()
-{
-let sueldo;
-let aumento;
-let resultado;
+    /*
+    Debemos lograr tomar el importe por ID ,
+    transformarlo a entero (parseInt), luego
+    mostrar el importe con un aumento del 10 %
+    en el cuadro de texto "RESULTADO".
+    Castillo Agustín Div X*/
+    function mostrarAumento()
+    {
+    let sueldo;
+    let aumento;
+    let resultado;
 
-sueldo = document.getElementById("txtIdSueldo").value;
+    sueldo = document.getElementById("txtIdSueldo").value;
 
-sueldo=parseInt(sueldo);
+    sueldo=parseInt(sueldo);
 
-aumento = sueldo * 10 / 100;
+    aumento = sueldo * 10 / 100;
 
-resultado = sueldo + aumento;
+    resultado = sueldo + aumento;
 
-document.getElementById("txtIdResultado").value = resultado;
+    document.getElementById("txtIdResultado").value = resultado;
 
-}
+    }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,23 +1,23 @@
-/*
-Debemos lograr tomar el importe por ID.
-Transformarlo a entero (parseInt), luego
-mostrar el importe con un Descuento del 25 %
-en el cuadro de texto "RESULTADO"
-Castillo Agustín Div X*/
-function mostrarAumento()
-{
-let importe;
-let descuento;
-let resultado;
+    /*
+    Debemos lograr tomar el importe por ID.
+    Transformarlo a entero (parseInt), luego
+    mostrar el importe con un Descuento del 25 %
+    en el cuadro de texto "RESULTADO"
+    Castillo Agustín Div X*/
+    function mostrarAumento()
+    {
+    let importe;
+    let descuento;
+    let resultado;
 
-importe = document.getElementById("txtIdImporte").value;
+    importe = document.getElementById("txtIdImporte").value;
 
-importe=parseInt(importe);
+    importe=parseInt(importe);
 
-descuento = importe * 25 / 100;
+    descuento = importe * 25 / 100;
 
-resultado = importe - descuento;
+    resultado = importe - descuento;
 
-document.getElementById("txtIdResultado").value = resultado;
+    document.getElementById("txtIdResultado").value = resultado;
 
-}
+    }
